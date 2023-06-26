e126f257be635aa1eb7b64dcd1a691acae488036
rakshithar142 <66309197+rakshithar142@users.noreply.github.com>
2023-06-26 18:20:20 +0530

    File based routing and navigating (#1)
    * cleanup
    
    * File based routing and Navigation

---
+++ app/search/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Searchbar from "../../components/Searchbar";
+
+export default function Header() {
+  return (
+    <div className="bg-gradient-to-r to-[#5f6984] from-[#0f1f47] p-2">
+      <div className="text-left text-lg py-3 m-auto flex justify-center">
+       <Searchbar />
+      </div>
+    </div>
+  );
+}
