6e962749ae113591b9361e2fc4d8cbd709224f5c
rakshithar142 <66309197+rakshithar142@users.noreply.github.com>
2023-06-27 23:32:38 +0530

    Fetching data in server components (#4)
    Fetching data in server components

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "./components/Header";
 import RestaurantNavBar from "./components/RestaurantNavBar";
 import Title from "./components/Title";
 import Rating from "./components/Rating";
@@ -7,16 +6,50 @@ import Description from "./components/Description";
 import Images from "./components/Images";
 import Reviews from "./components/Reviews";
 import ReservationCard from "./components/ReservationCard";
+import { PrismaClient } from "@prisma/client";
 
-export default function Restaurant() {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+  if (!restaurant) {
+    throw new Error();
+  }
+  return restaurant;
+};
+export default async function Restaurant({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar />
-        <Title />
+        <RestaurantNavBar slug={restaurant.slug}/>
+        <Title title={restaurant.name}/>
         <Rating />
-        <Description />
-        <Images />
+        <Description description={restaurant.description}/>
+        <Images images={restaurant.images}/>
         <Reviews />
       </div>
       <div className="w-[27%] relative text-reg">
