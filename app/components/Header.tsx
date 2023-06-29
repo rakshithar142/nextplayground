0d33c4210b21852f9655698fd199304ee436514e
rakshithar142 <rakshithar142@gmail.com>
2023-06-29 21:04:41 +0530

    Remove search bar component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,4 @@
-import SearchBar from "./SearchBar";
+// import SearchBar from "./SearchBar";
 
 export default function Header() {
   return (
@@ -7,7 +7,7 @@ export default function Header() {
         <h1 className="text-white text-5xl font-bold mb-2">
           Find your table for any occasion
         </h1>
-        <SearchBar />
+        {/* <SearchBar /> */}
       </div>
     </div>
   );
--- app/search/components/Header.tsx
+++ app/search/components/Header.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Searchbar from "../../components/SearchBar";
+// import Searchbar from "../../components/SearchBar";
 
 export default function Header() {
   return (
     <div className="bg-gradient-to-r to-[#5f6984] from-[#0f1f47] p-2">
       <div className="text-left text-lg py-3 m-auto flex justify-center">
-       <Searchbar />
+       {/* <Searchbar /> */}
       </div>
     </div>
   );
